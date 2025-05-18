7dfc8bd4ba590ce74e3d8598777d2cd85c47fc56
Rabiya <rabiya14.a@gmail.com>
2025-05-18 21:49:33 +0530

    new

--- num.c
+++ num.c
@@ -3,17 +3,14 @@
 int main() {
     int num1, num2, sum;
 
-    // Ask for input
-    printf("Enter first number: ");
+       printf("Enter first number: ");
     scanf("%d", &num1);
 
     printf("Enter second number: ");
     scanf("%d", &num2);
 
-    // Add the numbers
-    sum = num1 + num2;
+       sum = num1 + num2;
 
-    // Display the result
     printf("Sum = %d\n", sum);
 
     return 0;
